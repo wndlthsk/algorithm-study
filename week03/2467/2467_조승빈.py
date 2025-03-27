0f35a62fbf1b9f588e002474982f5c9d20636dc1
changeme4585 <benscience@naver.com>
2025-03-27 14:17:04 +0900

    백준 2467

---
+++ week03/2467/2467_조승빈.py
@@ -0,0 +1,25 @@
+N = int(input())
+arr = []
+
+arr = list(map(int,input().split(" ")))
+
+low = 0
+high = N-1
+a = arr[low]
+b = arr[high]
+answer = 9999999999999999
+while low < high:
+    if answer > abs(arr[low] + arr[high]):
+        answer = abs(arr[low] + arr[high])
+        a = arr[low]
+        b = arr[high]
+    if arr[low] + arr[high] < 0:
+        low+=1
+    elif arr[low] + arr[high] > 0:
+        high -=1
+    else:
+        a = arr[low]
+        b = arr[high]
+        break
+print(a,end=" ")
+print(b)
