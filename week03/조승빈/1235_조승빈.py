d59d3a61d99ec805b113ae15c7ff2670d8a91419
changeme4585 <benscience@naver.com>
2025-04-03 22:16:45 +0900

    7682

--- week03/조승빈/1235_조승빈.py
+++ week03/조승빈/1235_조승빈.py
@@ -2,10 +2,10 @@ arr = []
 N  = int(input())
 for _ in range(N):
     arr.append(input())
-for i in range(6,-1,-1):
+for i in range(len(arr[0])-1,-1,-1):
     s = set()
     for idx  in range(N):   
         s.add(arr[idx][i:])
     if len(s) == N:
-        print(7-i)
+        print(len(arr[0])-i)
         break
