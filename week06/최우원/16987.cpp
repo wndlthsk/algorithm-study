a7d972184fd2bba5eed61f34eb852cb2e66fef87
userwon-create <woowon1011@naver.com>
2025-04-20 17:28:58 +0900

    백준 16987

---
+++ week06/최우원/16987.cpp
@@ -0,0 +1,33 @@
+int n;
+int mx,cnt;
+int s[];
+int w[];
+
+void solve(int a){
+    if( a == n){
+        mx = max(mx,cnt);
+        return ;
+    }
+    for(int i = 0; i < n; i++){
+        if( i == a || s[a] <= 0 ) continue;
+        s[a] -= w[i];
+        s[i] -= w[a];
+        if (s[a] <= 0) cnt++;
+        if (s[i] <= 0) cnt++;
+        solve(a+1);
+        s[a] += w[i];
+        s[i] += w[a];
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> s[n] >> w[n] ;
+    }
+    solve(0);
+    cout << cnt ;
+}
