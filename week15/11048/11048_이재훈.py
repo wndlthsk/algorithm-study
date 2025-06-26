7fb070561d39ea9b9ea7a904492cd9e098af6a41
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-06-26 11:06:12 +0000

    백준 11048 수정

--- week15/11048/11048_이재훈.py
+++ week15/11048/11048_이재훈.py
@@ -9,8 +9,8 @@ for i in range(N):
     candy = board[i][j]
     
     can_go_1 = dp[i - 1][j] if i > 0 else 0
-    can_go_2 = dp[i][j - 1] if i > 0 else 0
-    can_go_3 = dp[i - 1][j - 1] if i > 0 else 0
+    can_go_2 = dp[i][j - 1] if j > 0 else 0
+    can_go_3 = dp[i - 1][j - 1] if i > 0 and j > 0 else 0
     
     dp[i][j] = max(can_go_1, can_go_2, can_go_3) + candy
 print(dp[N - 1][M - 1])
