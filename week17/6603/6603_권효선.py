ab6e141d82d85e71d90d8544ad9649c126aac3d6
h17237 <hyo6521@naver.com>
2025-07-10 21:15:15 +0900

    Update 6603_권효선.py

--- week17/6603/6603_권효선.py
+++ week17/6603/6603_권효선.py
@@ -1,1 +1,25 @@
+import sys
 
+input = sys.stdin.readline
+
+def dfs(dep, i):
+	if dep == 6: 
+		print(*ret)
+		return
+	
+	for i in range(i, k):
+		if dep + k - i < 6: 
+			return 해당 재귀문 종료
+		ret.append(s[i]) 
+		dfs(dep + 1, i + 1) # 재귀
+		ret.pop()
+
+while True:
+	g = list(map(int, input().split()))
+	k = g[0]
+	s = g[1:]
+	if k == 0:
+		break
+	ret = []
+	dfs(0, 0)
+	print()
