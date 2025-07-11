4cdfc8a4f4a8dc7b3af7b3d584a4f96eb658edf1
changeme4585 <benscience@naver.com>
2025-07-11 21:10:44 +0900

    백준 6603

--- week17/6603/6603_조승빈.py
+++ week17/6603/6603_조승빈.py
@@ -3,7 +3,7 @@ while True:
     arr = list(map(int,input().split(" ")))
     if arr[0]==0:
         break
-    num_len = arr.pop(0)
+    arr.pop(0)
     ans = list(combinations(arr,6))
     for an in ans:
         for a in an:
