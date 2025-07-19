8e66ee28846b1882c71c5df7fc140654d8f17e03
changeme4585 <benscience@naver.com>
2025-07-19 22:39:59 +0900

    백준 15989

---
+++ week18/15989/15989_조승빈.py
@@ -0,0 +1,9 @@
+N = int(input())
+arr = [0] * (10001)
+arr[0] = 1
+for i in range(1,4):
+    for idx in range(i,10001):
+        arr[idx] +=arr[idx - i]
+for _ in range(N):
+    print(arr[int(input())])
+    
