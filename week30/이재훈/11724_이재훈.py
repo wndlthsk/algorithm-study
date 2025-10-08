be5a378b4d0ecaa0559ca746ba410648ff031101
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-10-08 15:02:34 +0000

    백준 11724 수정

--- week30/이재훈/11724_이재훈.py
+++ week30/이재훈/11724_이재훈.py
@@ -1,14 +1,18 @@
+import sys
+input = sys.stdin.readline
 N, M = map(int, input().split())
-lines = [list(map(int, input().split())) for _ in range(M)]
-
 graph = {}
-for u, v in lines:
+
+for _ in range(M):
+  u, v = map(int, input().split())
   graph.setdefault(u, []).append(v)
   graph.setdefault(v, []).append(u)
 
 for i in range(1, N + 1):
   if i not in graph:
     graph[i] = []
+  else:
+    graph[i].sort()  # 탐색 효율 향상
 
 def dfs_stack(start, visited):
   stack = [start]
