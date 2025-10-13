63d2fde0a834cf6f8cbdcc4d5e41ba6f934ccbd6
hyunseo <wndlthsk@naver.com>
2025-10-13 23:57:49 +0900

    프로그래머스 이진변환반복하기

---
+++ week30/우현서/이진변환반복하기_우현서.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] solution(String s) {
+        int countChange = 0;
+        int countZero = 0;
+
+        while(true) {
+            if (s.equals("1")) break;
+
+            for (char c: s.toCharArray()) {
+                if (c == '0') {
+                    countZero++;
+                }
+            }
+
+            s = s.replace("0", "");
+            int c = s.length();
+            s = Integer.toBinaryString(c);
+            countChange++;
+        }
+
+        return new int[]{countChange, countZero};
+    }
+}
