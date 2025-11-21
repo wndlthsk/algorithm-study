e72f05b8cc9e12325336782a39166752d1778b4e
hyunseo <wndlthsk@naver.com>
2025-11-21 23:47:50 +0900

    프로그래머스 연속펄스부분수열의합

---
+++ week35/우현서/연속펄스부분수열의합_우현서.java
@@ -0,0 +1,34 @@
+// 연속 부분 수열에 같은 길이의 펄스 수열을 각 원소끼리 곱해서 연속 펄스 부분 수열
+// 합이 가장 큰 것을 리턴
+
+
+class Solution {
+    public long solution(int[] sequence) {
+        int n = sequence.length;
+        int[] pulse1 = new int[n];
+        int[] pulse2 = new int[n];
+        int x = 1;
+        for (int i=0; i<n; i++) {
+            pulse1[i] = x * sequence[i];
+            pulse2[i] = -1 * x * sequence[i];
+            x *= -1;
+        }
+        // System.out.println(Arrays.toString(pulse1));
+        // System.out.println(Arrays.toString(pulse2));
+
+        long answer = Math.max(kadane(pulse1), kadane(pulse2));
+
+        return answer;
+    }
+
+    public long kadane(int[] arr) {
+        long cur = arr[0];
+        long best = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            cur = Math.max(arr[i], cur + arr[i]);
+            best = Math.max(best, cur);
+        }
+        return best;
+    }
+}
