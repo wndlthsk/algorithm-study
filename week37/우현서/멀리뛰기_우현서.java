736631449a0ac5cbbb0e0f92524a71551d9231be
hyunseo <wndlthsk@naver.com>
2025-12-01 00:06:17 +0900

    프로그래머스 멀리뛰기

---
+++ week37/우현서/멀리뛰기_우현서.java
@@ -0,0 +1,17 @@
+// 1 or 2칸 뜀
+// dp[i] = dp[i-1] + dp[i-2], dp[n] 구하기
+
+class Solution {
+    public long solution(int n) {
+        if (n == 1) return 1;
+
+        long[] dp = new long[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i=3; i<=n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+        }
+
+        return dp[n];
+    }
+}
