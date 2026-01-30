0a68fd916dd8940c7b2724b0c79db52b5c00ce5a
hyunseo <wndlthsk@naver.com>
2026-01-30 19:13:30 +0900

    프로그래머스 카펫

---
+++ week44/카펫_우현서.java
@@ -0,0 +1,24 @@
+// 카펫의 가로 세로 크기 구해
+// 2x + 2y - 4 = brown,
+// (x-2) * (y-2) = yellow
+// x >= y
+// x+y = brown/2 +2
+
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = {};
+
+        int sum = brown / 2 + 2;
+        int y = 3;
+
+        while (true) {
+            int x = sum - y;
+
+            if ((x - 2) * (y - 2) == yellow) {
+                return new int[]{x, y};
+            }
+
+            y++;
+        }
+    }
+}
