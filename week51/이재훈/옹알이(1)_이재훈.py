3b41c328fe64c9a6dbb5ed805ce2fd9f7f4df14c
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2026-05-08 16:00:58 +0000

    프로그래머스 짝수와홀수, 평균구하기

--- week51/이재훈/옹알이(1)_이재훈.py
+++ week51/이재훈/옹알이(1)_이재훈.py
@@ -1,14 +1,22 @@
 def solution(babbling):
-    words = ["aya", "ye", "woo", "ma"]
-    answer = 0
+    announce = ["aya", "ye", "woo", "ma"]
     
+    cnt = 0
     for b in babbling:
-        temp = b
+        word = b
+        while len(word) > 0:
+            is_deleted = False
+            
+            for x in announce:
+                if word.startswith(x):
+                    word = word[len(x):]
+                    is_deleted = True
+                    break
+            
+            if not is_deleted:
+                break
         
-        for w in words:
-            temp = temp.replace(w, " ")
-        
-        if temp.strip() == "":
-            answer += 1
-    
-    return answer
+        if word == '':
+            cnt += 1
+            
+    return cnt
--- week51/이재훈/택배상자꺼내기_이재훈.py
+++ week51/이재훈/택배상자꺼내기_이재훈.py
@@ -1,26 +1,27 @@
 def solution(n, w, num):
-    # num의 row, col
-    r = (num - 1) // w
-    c = (num - 1) % w
+    total_r = (n - 1) // w + 1
+    total_c = w
     
-    # 지그재그 보정
-    if r % 2 == 1:
-        c = w - 1 - c
+    is_right = False
+    num_r = (num - 1) // w
+    if num_r % 2 == 0:
+        is_right = True
     
-    answer = 1  # 자기 자신 포함
+    base = (num - 1) % w
+    num_c = base if is_right else (w - 1) - base
     
-    # 위 row들 확인
-    for nr in range(r + 1, (n - 1) // w + 1):
-        # 해당 row의 col 계산
-        if nr % 2 == 0:
-            nc = c
-        else:
-            nc = w - 1 - c
-        
-        # 실제 번호 계산
-        box = nr * w + nc + 1
-        
-        if box <= n:
-            answer += 1
+    remain = n % w
+    if remain == 0:
+        return total_r - num_r
     
-    return answer
+    last_row = (n - 1) // w
+    
+    is_exist = False
+    if last_row % 2 == 0:
+        if 0 <= num_c < remain:
+            is_exist = True
+    else:
+        if w - remain <= num_c < w:
+            is_exist = True
+    
+    return total_r - num_r if is_exist else total_r - num_r - 1
