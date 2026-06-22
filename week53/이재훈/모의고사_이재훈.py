a9c82576113c0ceb0cb3196c37ada020ac88432f
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2026-06-22 14:53:06 +0000

    프로그래머스 폰켓몬, 모의고사

---
+++ week53/이재훈/모의고사_이재훈.py
@@ -0,0 +1,17 @@
+def solution(answers):
+    patterns = [
+        [1, 2, 3, 4, 5],
+        [2, 1, 2, 3, 2, 4, 2, 5],
+        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    ]
+    
+    scores = [0, 0, 0]
+    
+    for i, answer in enumerate(answers):
+        for j in range(3):
+            if answer == patterns[j][i % len(patterns[j])]:
+                scores[j] += 1
+    
+    max_score = max(scores)
+    
+    return [i + 1 for i, score in enumerate(scores) if score == max_score]
